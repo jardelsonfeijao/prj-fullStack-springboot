549cfc4e70f34a25955012977dce0ff4787c070e
jardelsonfeijao <jardelsonfeijao04@gmail.com>
2025-05-12 09:53:36 -0300

    feat(startup): adicionar instanciação de categorias na classe JfSpringApplication
    Cria objetos Categoria e os salva com categoriaRepository.saveAll(...) na inicialização do projeto

--- src/main/java/com/jf/exemplo/JfSpringApplication.java
+++ src/main/java/com/jf/exemplo/JfSpringApplication.java
@@ -1,13 +1,35 @@
 package com.jf.exemplo;
 
+import com.jf.exemplo.domain.Categoria;
+import com.jf.exemplo.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class JfSpringApplication {
+import java.util.Arrays;
+import java.util.List;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class JfSpringApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+    public JfSpringApplication(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(JfSpringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(List.of(cat1, cat2));
+
+	}
 }
